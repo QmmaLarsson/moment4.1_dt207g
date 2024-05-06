e9fcc98e5cb9e2e67fa0c7078f6caf2efa26f064
Emma Larsson <emma940112@gmail.com>
2024-05-06 07:55:43 +0200

    Kopplat webbtjänsten till en MongoDB-databas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Anslut till MongoDB
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Ansluten till MongoDB");
+}).catch((error) => {
+    console.error("Error vid anslutning till MongoDB")
+});
+
+//Användar-model
+const user = require("../models/user");
+const User = require("../models/user");
 
 //Lägg till ny användare
 router.post("/register", async (req, res) => {
@@ -12,6 +26,8 @@ router.post("/register", async (req, res) => {
         }
 
         //Korrekt input - Skapa användare
+        const user = new User({ username, password });
+        await user.save();
         res.status(201).json({ message: "Användare skapad" })
 
     } catch (error) {
@@ -30,12 +46,18 @@ router.post("/login", async (req, res) => {
         }
 
         //Kontrollera username och password
-        if (username === "Emma" && password === "Anton") {
-            res.status(200).json({ message: "Inloggningen lyckades" });
-        } else {
-            res.status(401).json({ error: "Ogitligt användarnamn och lösenord" });
+        //Kontrollera om användaren finns
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ error: "Felaktigt användarnamn eller lösenord" });
         }
-
+        //Kontrollera lösenord
+        const isPasswordMatch = await user.comparePassword(password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({ error: "Felaktigt användarnamn eller lösenord" });
+        } else {
+            res.status(200).json({ message: "Inloggningen lyckades" })
+        }
     } catch (error) {
         res.status(500).json({ error: "Server error" });
     }
